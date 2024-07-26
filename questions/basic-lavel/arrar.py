b0aff6615e0c4c654f2fbbb2115dcb4ec47cd629
Xharry01X <harshitsingh50621@gmail.com>
2024-07-27 01:21:53 +0530

    learning array

---
+++ questions/basic-lavel/arrar.py
@@ -0,0 +1,24 @@
+fruits = ["banana","apple","mango"]
+
+# for i in fruits:
+    
+#     print(i)
+    
+# Loop through each index
+for i in enumerate(fruits):
+    print(f"index {i} : {fruits}")
+    
+    
+# nested for loop
+colors = ["red","green","black"]
+fruits = ["apple","banana","cherry"]
+
+# nesting start here
+for color in colors:
+    for fruit in fruits:
+        print(f"{color} {fruit}")
+        
+        
+# star pattern
+
+        
